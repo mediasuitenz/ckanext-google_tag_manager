e24f9084a60fbd1d6739362ad965cd7c98e74699
Niall McAndrew <niall@mediasuite.co.nz>
2021-08-04 17:30:05 +1200

    Fix python 2/3 compatibility.

--- ckanext/google_tag_manager/helpers.py
+++ ckanext/google_tag_manager/helpers.py
@@ -1,8 +1,8 @@
 from ckan.common import config
-from builtins import str
+from six import text_type
 
 
 def get_gtm_code():
     # To get Google Tag Manager Code
     gtm_code = config.get('ckan.google_tag_manager.gtm_container_id', False)
-    return str(gtm_code)
+    return text_type(gtm_code)
